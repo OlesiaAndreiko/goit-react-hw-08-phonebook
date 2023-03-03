768003bbd8a29fd8310c8294e65f4295a73d2c51
Olesia Andreiko <olesikmail@gmail.com>
2023-03-03 19:32:49 +0200

    Fix styles.

--- src/components/Heading/Heading.styled.jsx
+++ src/components/Heading/Heading.styled.jsx
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const Title = styled.h2`
     text-align: center;
-    font-size: 35px;
+    font-size: 28px;
     font-weight: 600;
+    line-height: calc(18 / 14);
+    letter-spacing: 0.02em;
     text-transform: capitalize;
     margin: 0;
     padding: 10px;
